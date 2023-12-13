b372082c3a593a6abd1ceaea3e34e8e7d80dbb3e
Rafael Victor <rafaael.vrm@gmail.com>
2023-12-13 20:39:52 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/Rafa-light.png")
+
+    //se tiver se o light mode, manter a imagem normal
+  } else {
+    img.setAttribute("src", "./assets/Rafa.png")
+  }
+}
